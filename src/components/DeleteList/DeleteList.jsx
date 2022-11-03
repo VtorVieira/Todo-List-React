845efe7eeb997d7532770d36a45b422e7580f261
vtorvieira <vieira.vitor42@gmail.com>
2022-11-03 17:20:43 -0300

    feat: add line-through checked

--- src/components/DeleteList/DeleteList.jsx
+++ src/components/DeleteList/DeleteList.jsx
@@ -1,17 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TodoContext from '../../context/TodoContext';
 import { MainDiv } from './Style';
 
 function DeleteList() {
   const { setLog, setList, list, log } = useContext(TodoContext);
+  const [checked, setChecked] = useState([]);
+  // console.log('checked', checked);
   const current = new Date();
 
   const date = `
-  ${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()} 
-  Hora: ${current.getHours()}:${current.getMinutes()}
+    ${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()} 
+    Hora: ${current.getHours()}:${current.getMinutes()}
   `;
 
   const removeList = (id, itemSelected) => {
+    if (checked.includes(id)) {
+      return setChecked(checked.filter((i) => i !== id));
+    }
+
     // identifica a posição do item dentro do array,
     // e retorna um novo array.
     const removeItem = list.filter((_, index) => index !== id);
@@ -21,13 +27,30 @@ function DeleteList() {
     setList(removeItem);
   };
 
+  const handleChange = (index) => {
+    if (checked.includes(index)) {
+      return setChecked(checked.filter((i) => i !== index));
+    } setChecked([...checked, index]);
+  };
+
   return (
     <MainDiv>
       <ol>
         {list && list.map((itens, index) => (
           <div key={index} id='Mainlist'>
-            <div id="currentList">
-              <li className="list">{itens}</li>
+            <div
+              id="currentList"
+              style={{
+                textDecoration: checked.includes(index) ? 'line-through' : 'none'
+              }}
+            >
+              <input
+                type="checkbox"
+                name={`checkbox${index}`}
+                value={`checkbox${index}`}
+                onChange={() => handleChange(index)}
+              />
+              {itens}
             </div>
             <div id="buttonDelete">
               <button className="btn" type="button" onClick={() => removeList(index, itens)} >Deletar</button>
